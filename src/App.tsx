7624778e43bccdecd33d3ba81177e6d2a8ae45a7
JuanFila <juanfilapablo@hotmail.com>
2024-04-28 23:54:47 -0300

    new project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from "./components/Button"
+
+
+function App() {
+
+  return (
+    <>
+      <Button variant="primary"/>
+      <Button variant="secondary"/>
+      <Button variant="success"/>
+      <Button variant="danger"/>
+      <Button/>
+    </>
+  )
+}
+
+export default App
