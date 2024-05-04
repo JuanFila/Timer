67b267d85f230252968581130b5246194959c802
JuanFila <juanfilapablo@hotmail.com>
2024-05-04 00:22:28 -0300

    update home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,13 @@
 import { Play } from "phosphor-react";
-import { CountDownContainer, FormContainer, HomeContainer, Separator } from "./style";
+import {
+  CountDownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+  StartCountDown,
+  TaskAmountInput,
+  TaskInput,
+} from "./style";
 
 export function Home() {
   return (
@@ -7,10 +15,14 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" type="text" />
+          <TaskInput
+            id="task"
+            type="text"
+            placeholder="Dê um nome para seu projeto"
+          />
 
           <label htmlFor="minutesAmount">durante</label>
-          <input id="minutesAmount" type="number" />
+          <TaskAmountInput id="minutesAmount" type="number" placeholder="00" />
 
           <span>minutos.</span>
         </FormContainer>
@@ -23,10 +35,10 @@ export function Home() {
           <span>0</span>
         </CountDownContainer>
 
-        <button type="submit">
-          <Play size={24}/>
+        <StartCountDown type="submit">
+          <Play size={24} />
           Começar
-        </button>
+        </StartCountDown>
       </form>
     </HomeContainer>
   );
--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -24,7 +24,7 @@ export const FormContainer = styled.div`
   gap: 0.5rem;
 
   color: ${(props) => props.theme["gray-100"]};
-  font-size: 1.25rem;
+  font-size: 1.125rem;
   font-weight: bold;
   flex-wrap: wrap;
 `;
@@ -53,3 +53,60 @@ export const Separator = styled.div`
   display: flex;
   justify-content: center;
 `;
+
+export const StartCountDown = styled.button`
+width: 100%;
+border: 0;
+border-radius: 8px;
+
+padding: 1rem;
+
+display: flex;
+align-items: center;
+justify-content: center;
+
+gap: 0.5rem;
+font-weight: bold;
+cursor: pointer;
+
+background: ${props => props.theme['green-500']};
+color: ${(props) => props.theme["gray-100"]};
+transition: 1s;
+
+&:disabled{
+  opacity: 0.7;
+  cursor: not-allowed;
+}
+
+&:not(:disabled):hover{
+  background: ${props => props.theme['green-700']}
+}
+`
+
+const BaseInput = styled.input`
+  background: transparent;
+  border: 0;
+  height: 2.5rem;
+  border-bottom: 2px solid ${props => props.theme['gray-500']};
+  font-weight: bold;
+  font-size: inherit;
+  padding: 0 0.5rem;
+
+  &:focus{
+    box-shadow: none;
+    border-bottom: 2px solid ${props => props.theme['green-500']};
+  }
+
+  &::placeholder {
+    color: ${props => props.theme['gray-500']};
+  }
+
+ 
+`
+
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+`
+export const TaskAmountInput = styled(BaseInput)`
+  width: 4rem;
+`
