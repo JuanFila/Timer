fcbb42253de358fbe88b5b0c88864c1cdd209e3a
JuanFila <juanfilapablo@hotmail.com>
2024-05-06 22:21:51 -0300

    add zod + hook form lib

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,11 +8,37 @@ import {
   TaskAmountInput,
   TaskInput,
 } from "./style";
+import { useForm } from 'react-hook-form';
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as Zod from 'zod';
 
+const newCycleFormValidationSchema = Zod.object({
+   task: Zod.string().min(1, 'informe  a tarda'),
+   minutesAmount: Zod.number().min(5, 'o  ciclo mínimo é de 5 minutos').max(60, 'o ciclo máximo é de 60 minutos'),
+})
+
+type newCycleFormData = Zod.infer<typeof newCycleFormValidationSchema>
 export function Home() {
+
+  const { register, handleSubmit, watch, reset} = useForm<newCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    }
+  })
+
+  function handleCreateNewCycle(data: any) {
+    console.log(data)
+    reset();
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task;
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
@@ -20,6 +46,7 @@ export function Home() {
             type="text"
             placeholder="Dê um nome para seu projeto"
             list="task-suggestions"
+            {...register('task')}
           />
 
           <datalist id="task-suggestions">
@@ -37,6 +64,7 @@ export function Home() {
           step={5}
           min={5}
           max={60}
+          {...register('minutesAmount', { valueAsNumber: true})}
           />
 
           <span>minutos.</span>
@@ -50,7 +78,7 @@ export function Home() {
           <span>0</span>
         </CountDownContainer>
 
-        <StartCountDown type="submit">
+        <StartCountDown disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Começar
         </StartCountDown>
